7f9b15a1ca39ddfc571189af5ace32e7b8c45cf3
Svetlana Razheva <rajevasvetlana@gmail.com>
2020-01-11 18:43:12 +0200

    Add gitignore. Add base webpack config

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-console.warn('I am a Star Trek nerd');
-console.log('So through out this tutorial, you will see a lot of Star Trek quotes');
-console.log('Starting now');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
+        have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
+        to prove it.
+      </p>
+      <p>- Captain Kirk</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('app'));
