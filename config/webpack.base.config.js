4a4b0e868f843f04d8bea364110df6e52aa0609b
Svetlana Razheva <rajevasvetlana@gmail.com>
2020-01-11 22:02:40 +0200

    npm run prebuild

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -1,6 +1,7 @@
 const webpack = require("webpack");
 const merge = require("webpack-merge");
 
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
@@ -32,6 +33,7 @@ module.exports = env => {
             template: './src/index.html',
             filename: './index.html'
           }),
+          new CopyWebpackPlugin([ { from: 'src/static' } ]),
           new webpack.DefinePlugin({
             'process.env.VERSION': JSON.stringify(env.VERSION),
             'process.env.PLATFORM': JSON.stringify(env.PLATFORM)
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import './stylesheets/application.scss';
 const App = () => {
   return (
     <div className="app">
+      <img alt="header" src="/dist/images/header.jpeg" className="app-header" />
       <p>
         We are a most promising species, Mr. Spock, as predators go. Did you know that? I frequently
         have my doubts. I dont. Not any more. And maybe in a thousand years or so, we will be able
