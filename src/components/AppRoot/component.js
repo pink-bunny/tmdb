5b8184c255e1594ca5a184a9d018db4bba469272
Svetlana Razheva <rajevasvetlana@gmail.com>
2020-01-14 14:12:29 +0200

    Fix linters error.

--- src/components/AppRoot/component.js
+++ src/components/AppRoot/component.js
@@ -1,35 +1,24 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Header from '../Header';
 import NotFound from '../../pages/NotFound';
 
-const AppRoot = () => {
-  return (
-    <Router>
-      <Header />
+const Home = () => <div className="container"><h2>Home</h2></div>;
+const About = () => <div className="container"><h2>About</h2></div>;
 
-      <div className="main-body">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+const AppRoot = () => (
+  <Router>
+    <Header />
 
-const Home = () => {
-  return <div className="container"><h2>Home</h2></div>;
-};
-
-const About = () => {
-  return <div className="container"><h2>About</h2></div>;
-};
+    <div className="main-body">
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>
+);
 
 export default AppRoot;
--- src/components/Header/component.js
+++ src/components/Header/component.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Header = () => (
   <header className="main-header">
@@ -15,7 +15,7 @@ const Header = () => (
         </ul>
       </nav>
     </div>
-  </ header>
+  </header>
 );
 
 export default Header;
