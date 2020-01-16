8c64fdaac624c9349371d98118d7a2986d4664d6
pink-bunny <rajevasvetlana@gmail.com>
2020-01-16 15:32:32 +0200

    Feature/linter (#3)
    * Add eslint.
    
    * Fix linters error.
    
    * Add command for eslint.

--- src/pages/NotFound/component.js
+++ src/pages/NotFound/component.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
-const NotFound = () => (
-  <div className="container">
-    <h2>No match for <code>{location.href}</code></h2>
-  </div>
-);
+const NotFound = () => {
+  const location = useLocation();
+
+  return (
+    <div className="container">
+      <h2>
+        No match for
+        <code>{location.pathname}</code>
+      </h2>
+    </div>
+  );
+};
 
 export default NotFound;
