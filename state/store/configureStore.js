1576efe755193ccc3e90721184e0287e4b68b411
Svetlana Razheva <rajevasvetlana@gmail.com>
2020-01-17 14:28:44 +0200

    redux-devtools-extension basic store

--- state/store/configureStore.js
+++ state/store/configureStore.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
 import reducer from './reducer';
 
-const configureStore = () => createStore(reducer);
+/* eslint-disable no-underscore-dangle */
+const configureStore = () => createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+/* eslint-enable */
 
 export default configureStore;
