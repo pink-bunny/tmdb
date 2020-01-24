3d6f852818727a2c2539ec529925a9b2b8015f2e
Svetlana Razheva <rajevasvetlana@gmail.com>
2020-01-24 09:45:00 +0200

    Create selector for isUserLoggedIn

--- src/pages/Home/container.js
+++ src/pages/Home/container.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 
 import Login from '../Login';
 import DashboardEmpty from '../DashboardEmpty';
+import { isUserLoggedIn } from '../../../state/session/selectors';
 
 const Home = ({ userLoggedIn }) => (
   userLoggedIn ? <DashboardEmpty /> : <Login />
@@ -13,8 +14,8 @@ Home.propTypes = {
   userLoggedIn: PropTypes.string.isRequired
 };
 
-const mapStateToProps = (state) => (
-  { userLoggedIn: state.sessionReducer.sessionId }
-);
+const mapStateToProps = (state) => ({
+  userLoggedIn: isUserLoggedIn(state)
+});
 
 export default connect(mapStateToProps)(Home);
