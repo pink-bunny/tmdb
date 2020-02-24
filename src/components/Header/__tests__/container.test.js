ffbf1002cf241a9d78091360b2e379378ba21c8a
Svetlana Razheva <rajevasvetlana@gmail.com>
2020-02-24 10:09:52 +0200

    Header container test.

--- src/components/Header/__tests__/container.test.js
+++ src/components/Header/__tests__/container.test.js
@@ -3,38 +3,18 @@ import configureStore from 'redux-mock-store';
 import { shallow } from 'enzyme';
 
 import ConnectedHeader from '../container';
-import { completeSession } from '../../../../state/session/actions';
 
 describe('Header container', () => {
-  let wrapper;
-  let store;
+  const initialState = {
+    session: {
+      sessionId: '123456',
+      username: 'John Doe'
+    }
+  };
+  const store = configureStore()(initialState);
+  const wrapper = shallow(<ConnectedHeader store={store} />).dive();
 
-  beforeEach(() => {
-    const initialState = {
-      session: {
-        sessionId: '123456',
-        username: 'John Doe',
-        userLoggedIn: true
-      }
-    };
-    store = configureStore()(initialState);
-    wrapper = shallow(<ConnectedHeader store={store} />).dive();
-    store.dispatch(completeSession());
-  });
-
-
-  it('should dispatch completeSession action', () => {
-    const actions = store.getActions();
-    const expectedPayload = { type: 'COMPLETE_SESSION' };
-
-    expect(actions).toEqual([expectedPayload]);
-  });
-
-  it('mapStateToProps should contain username equals "John Doe"', () => {
-    expect(wrapper.props().store.getState().session.username).toMatch('John Doe');
-  });
-
-  it('mapStateToProps should contain userLoggedIn equals true', () => {
-    expect(wrapper.props().store.getState().session.userLoggedIn).toBe(true);
+  it('matches snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
   });
 });
